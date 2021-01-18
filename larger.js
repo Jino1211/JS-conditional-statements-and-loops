9fb3b349da45aeb78bddc956e7513911f165bc8b
Jino1211 <yone543@gmail.com>
2021-01-18 09:13:46 +0200

    larger

---
+++ larger.js
@@ -0,0 +1,10 @@
+const num1 = prompt("Input firs number");
+const num2 = prompt("Input second number");
+const max = Math.max(num1, num2);
+if (num1 === num2) {
+    console.log("The numbers is equals");
+}
+else {
+    console.log("The larger number between "+ num1 +"and"+num2+" is " + max);
+}
+   

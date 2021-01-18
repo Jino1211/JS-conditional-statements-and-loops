52b0511d6650b04bcc7788534e6745af6e0cf043
Jino1211 <yone543@gmail.com>
2021-01-18 13:06:56 +0200

    q4 with html

---
+++ q4.js
@@ -0,0 +1,20 @@
+const num1 = 2;
+const num2 = 4;
+const num3 = 1;
+const num4 = -8;
+const num5 = -2;
+if (num1>num2 && num1>num3 && num1>num4 && num1>num5) {
+    alert (num1);
+}
+if (num2>num1 && num2>num3 && num2>num4 && num2>num5) {
+    alert (num2);
+}
+if (num3>num2 && num3>num1 && num3>num4 && num3>num5) {
+    alert (num3);
+}
+if (num4>num2 && num4>num3 && num4>num1 && num4>num5) {
+    alert (num4);
+}
+if (num5>num2 && num5>num3 && num5>num4 && num5>num1) {
+    alert (num5);
+}
